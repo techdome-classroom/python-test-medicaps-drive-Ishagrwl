cb8cbb86e552909384f357346ad600ab21192659
mahakagr <66478886+mahakagr@users.noreply.github.com>
2024-04-25 14:02:45 +0530

    First Commit

--- program1.py
+++ program1.py
@@ -4,6 +4,24 @@ class Solution(object):
         :type s: str
         :rtype: bool
         """
+        if not s:
+            return True
+        
+        stack = []
+        parentheses_map = {')': '(', ']': '[', '}': '{'}
+        
+        for char in s:
+            if char in parentheses_map.values():
+                stack.append(char)
+            elif char in parentheses_map.keys():
+                if not stack or stack[-1] != parentheses_map[char]:
+                    return False
+                stack.pop()
+            else:
+                return False
+        
+        return not stack if stack else True
+
         pass
     
 
--- program2.py
+++ program2.py
@@ -4,4 +4,25 @@ class Solution(object):
         :type s: str
         :rtype: int
         """
+        roman_dict = {
+            'I': 1,
+            'V': 5,
+            'X': 10,
+            'L': 50,
+            'C': 100,
+            'D': 500,
+            'M': 1000
+        }
+
+        result = 0
+        prev_value = 0
+        for char in s[::-1]:
+            value = roman_dict[char]
+            if value < prev_value:
+                result -= value
+            else:
+                result += value
+            prev_value = value
+
+        return result
         pass
